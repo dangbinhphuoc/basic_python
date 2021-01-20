ddce745dd2900af8736df59088a7e24878abfb52
Boon Real Kua <64570049+dangbinhphuoc@users.noreply.github.com>
2021-01-20 20:58:54 +0700

    Add files via upload

---
+++ 3.Chuoi.py
@@ -0,0 +1,21 @@
+#các kí tự đặc biệt " nhưng nó sẽ lỗi do print tự nhận kí tự này là kí tự đặc biệt
+#ví dụ: print("chào mừng bạn đến bài "chuỗi"")
+#dưới đây là một số cách
+#để sử dụng được chúng ta sử dụng kí tự \ trước kí tự đặc biệt
+print("chào mừng bạn đến bài \"chuỗi\"")
+#tương tự vậy ta cũng có thể áp dụng với kí tự đặc biệt '
+print('chào mừng bạn đến bài \'chuỗi\'')
+#hoặc sử dụng đối của nó
+print('chào mừng bạn đến bài "chuỗi"')
+print("chào mừng bạn đến bài 'chuỗi'")
+
+#một số kí tự đặc biệt khác
+print("chào \n mừng bạn đến bài chuỗi")
+#\n -> xuống dòng
+print("chào \t mừng bạn đến bài chuỗi")
+#\t -> tab
+print("chào \a mừng bạn đến bài chuỗi")
+#\a -> chuông cảnh báo
+print("chào \bmừng bạn đến bài chuỗi")
+#\b -> xoá bỏ khoảng trắng phía trước nó
+
